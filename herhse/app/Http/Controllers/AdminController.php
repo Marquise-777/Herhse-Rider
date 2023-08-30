0afcb1f44f45b7dcbba04bb030b67cfe03008035
Marquise-777 <79907043+Marquise-777@users.noreply.github.com>
2023-08-30 18:14:14 +0530

    Herhse

---
+++ herhse/app/Http/Controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    //
+    // public function getRegistrationForm(){
+    //     // $user = Auth::user();
+    //     // dd($user[0]->id);
+    //     return view('auth.register');
+    // }
+}
